9b8b7d576c44f886d4919943b838ab52d3eb02ca
David Plajner <p_davek@blocked.local>
2026-08-05 14:40:01 +0200

    2.0.5

--- lombik/templates/createapp/blueprints/core/routes.py
+++ lombik/templates/createapp/blueprints/core/routes.py
@@ -7,7 +7,7 @@ def home():
     from datetime import datetime, timezone
     context = {
         "selected": "home",
-        "version": "2.0.4",
+        "version": "2.0.5",
         "current_ts": datetime.now(timezone.utc)
     }
     return render_template("/core/home.html", **context)
